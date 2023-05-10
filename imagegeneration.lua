1303398853e2d325c7bec94120798b1a9d481a32
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-09 21:45:48 -0600

    checkered pattern

--- imagegeneration.lua
+++ imagegeneration.lua
@@ -1,21 +1,28 @@
 local file = io.open("asdf.ppm", "w")
-local WIDTH = 256;
-local HEIGHT = 256;
+
+local WIDTH = 1024;
+local HEIGHT = 1024;
+
+function checkeredPattern(arr, x, y)
+    arr[#arr + 1] = x % 2 == y % 2 and "255 255 255" or "0 0 0"
+    arr[#arr + 1] = " "
+end
+
+function loop2d(arr, fun)
+    for y = 1, HEIGHT do
+        for x = 1, WIDTH do
+            fun(arr, x, y)
+        end
+    end
+    return arr
+end
+
 
 local tab = {}
 tab[#tab + 1] = "P3\n"
 tab[#tab + 1] = WIDTH .. " " .. HEIGHT .. "\n"
 tab[#tab + 1] = 255 .. "\n"
 
-for y = 1, HEIGHT do
-    for x = 1, WIDTH do
-        tab[#tab + 1] = x % 2 == y % 2 and "255 255 255" or "0 0 0"
-        tab[#tab + 1] = " " .. " "
-        -- tab[#tab + 1] = math.random(255) .. " "
-        -- tab[#tab + 1] = math.random(255) .. " "
-        -- tab[#tab + 1] = math.random(255) .. " " .. " "
-    end
-end
-
-file:write(table.concat(tab, ""))
+loop2d(tab, checkeredPattern)
+file:write(table.concat(tab, " "))
 file:close()
