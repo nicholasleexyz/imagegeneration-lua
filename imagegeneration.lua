61ad8f0a7a38b94c7a7df1c5915ee573d45fea6b
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-11 00:01:41 -0600

    format fix

--- imagegeneration.lua
+++ imagegeneration.lua
@@ -1,17 +1,59 @@
 require "ppm"
 
-function example_colorful_stripes()
-    local w = 260 * 2
-    local h = 260 * 2
-    local b = Bitmap.new(w, h)
-    -- b:fill(2, 2, 18, 18, {240,240,240})
-    b:setPixel(0, 15, {255, 68, 0})
-    for i = 1, w do
-        for j = 1, h do
-            b:setPixel(i, j, {(i + j * 8) % 256, (j + (255 * i)) % 256, (i * j) % 256});
+local WIDTH = 256
+local HEIGHT = 256
+
+function checkerboard()
+    local w = WIDTH
+    local h = HEIGHT
+    local b = Bitmap(w, h)
+    for y = 1, h do
+        for x = 1, w do
+            b[y][x] = x % 2 == y % 2 and {255, 255, 255} or {0, 0, 0}
         end
     end
     return b
 end
 
-example_colorful_stripes():writeP6('p6.ppm')
+function BitmapToPPM(bitmap, f)
+    local file = io.open(f, 'w')
+    local header = ("P3 %d %d 255\n"):format(WIDTH, HEIGHT)
+    file:write(header)
+    for y = 1, HEIGHT do
+        for x = 1, WIDTH do
+            local pixel = bitmap[y][x]
+            for i = 1, #pixel do
+                file:write(pixel[i])
+                file:write(" ")
+            end
+        end
+        file:write(" ")
+        file:write(" ")
+    end
+    io.close()
+end
+
+-- function save(data, filename)
+--     local file = io.open(filename, 'w')
+--     file:write(data)
+--     io.close(file)
+-- end
+
+BitmapToPPM(checkerboard(), "asdf.ppm")
+
+-- checkerboard():writeP6("checkerboard.txt")
+-- function example_colorful_stripes()
+--     local w = 260 * 2
+--     local h = 260 * 2
+--     local b = Bitmap.new(w, h)
+--     -- b:fill(2, 2, 18, 18, {240,240,240})
+--     b:setPixel(0, 15, {255, 68, 0})
+--     for i = 1, w do
+--         for j = 1, h do
+--             b:setPixel(i, j, {(i + j * 8) % 256, (j + (255 * i)) % 256, (i * j) % 256});
+--         end
+--     end
+--     return b
+-- end
+
+-- example_colorful_stripes():writeP6('p6.ppm')
--- ppm.lua
+++ ppm.lua
@@ -1,85 +1,97 @@
--- helper function, simulates PHP's array_fill function
-local array_fill = function(vbegin, vend, value)
-    local t = {}
-    for i = vbegin, vend do
-        t[i] = value
-    end
-    return t
-end
-
-Bitmap = {}
-Bitmap.__index = Bitmap
-
-function Bitmap.new(width, height)
-    local self = {}
-    setmetatable(self, Bitmap)
-    local white = array_fill(0, width, {255, 255, 255})
-    self.data = array_fill(0, height, white)
-    self.width = width
-    self.height = height
-    return self
-end
-
-function Bitmap:writeRawPixel(file, c)
-    local dt
-    dt = string.format("%c", c)
-    file:write(dt)
-end
-
-function Bitmap:writeComment(fh, ...)
-    local strings = {...}
-    local str = ""
-    local result
-    for _, s in pairs(strings) do
-        str = str .. tostring(s)
-    end
-    result = string.format("# %s\n", str)
-    fh:write(result)
-end
-
-function Bitmap:writeP6(filename)
-    local fh = io.open(filename, 'w')
-    if not fh then
-        error(string.format("failed to open %q for writing", filename))
-    else
-        fh:write(string.format("P6 %d %d 255\n", self.width, self.height))
-        self:writeComment(fh, "automatically generated at ", os.date())
-        for _, row in pairs(self.data) do
-            for _, pixel in pairs(row) do
-                self:writeRawPixel(fh, pixel[1])
-                self:writeRawPixel(fh, pixel[2])
-                self:writeRawPixel(fh, pixel[3])
-            end
+function Bitmap(width, height)
+    local b = {}
+    for y = 1, height do
+        b[y] = {}
+        for x = 1, width do
+            b[y][x] = {0,0,0}
         end
     end
+    return b
 end
 
--- function Bitmap:fill(x, y, width, height, color)
---     width = (width == nil) and self.width or width
---     height = (height == nil) and self.height or height
---     width = width + x
---     height = height + y
---     for i = y, height do
---         for j = x, width do
---             self:setPixel(j, i, color)
+-- -- helper function, simulates PHP's array_fill function
+-- local array_fill = function(vbegin, vend, value)
+--     local t = {}
+--     for i = vbegin, vend do
+--         t[i] = value
+--     end
+--     return t
+-- end
+
+-- Bitmap = {}
+-- Bitmap.__index = Bitmap
+
+-- function Bitmap.new(width, height)
+--     local self = {}
+--     setmetatable(self, Bitmap)
+--     local white = array_fill(0, width, {255, 255, 255})
+--     self.data = array_fill(0, height, white)
+--     self.width = width
+--     self.height = height
+--     return self
+-- end
+
+-- function Bitmap:writeRawPixel(file, c)
+--     local dt
+--     dt = string.format("%c", c)
+--     file:write(dt)
+-- end
+
+-- function Bitmap:writeComment(fh, ...)
+--     local strings = {...}
+--     local str = ""
+--     local result
+--     for _, s in pairs(strings) do
+--         str = str .. tostring(s)
+--     end
+--     result = string.format("# %s\n", str)
+--     fh:write(result)
+-- end
+
+-- function Bitmap:writeP6(filename)
+--     local fh = io.open(filename, 'w')
+--     if not fh then
+--         error(string.format("failed to open %q for writing", filename))
+--     else
+--         -- fh:write(string.format("P6 %d %d 255\n", self.width, self.height))
+--         fh:write(string.format("P3 %d %d 255\n", self.width, self.height))
+--         -- self:writeComment(fh, "automatically generated at ", os.date())
+--         for _, row in pairs(self.data) do
+--             for _, pixel in pairs(row) do
+--                 self:writeRawPixel(fh, pixel[1])
+--                 self:writeRawPixel(fh, pixel[2])
+--                 self:writeRawPixel(fh, pixel[3])
+--             end
 --         end
 --     end
 -- end
 
-function Bitmap:setPixel(x, y, color)
-    if x >= self.width then
-        -- error("x is bigger than self.width!")
-        return false
-    elseif x < 0 then
-        -- error("x is smaller than 0!")
-        return false
-    elseif y >= self.height then
-        -- error("y is bigger than self.height!")
-        return false
-    elseif y < 0 then
-        -- error("y is smaller than 0!")
-        return false
-    end
-    self.data[y][x] = color
-    return true
-end
+-- -- function Bitmap:fill(x, y, width, height, color)
+-- --     width = (width == nil) and self.width or width
+-- --     height = (height == nil) and self.height or height
+-- --     width = width + x
+-- --     height = height + y
+-- --     for i = y, height do
+-- --         for j = x, width do
+-- --             self:setPixel(j, i, color)
+-- --         end
+-- --     end
+-- -- end
+
+-- function Bitmap:setPixel(x, y, color)
+--     if x >= self.width then
+--         -- error("x is bigger than self.width!")
+--         return false
+--     elseif x < 0 then
+--         -- error("x is smaller than 0!")
+--         return false
+--     elseif y >= self.height then
+--         -- error("y is bigger than self.height!")
+--         return false
+--     elseif y < 0 then
+--         -- error("y is smaller than 0!")
+--         return false
+--     end
+--     self.data[y][x] = color
+--     return true
+-- end
